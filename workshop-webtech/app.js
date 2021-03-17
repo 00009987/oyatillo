a848244ad0c07ced690d68272b9f8bccf8b9134b
sjamoliddin <sjamolbek@mail.ru>
2021-03-17 12:46:18 +0500

    fixed all the bugs

--- workshop-webtech/app.js
+++ workshop-webtech/app.js
@@ -1,60 +1,57 @@
-const express = require("express");
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
 const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
-const fs = require("fs");
+const bodyParser = require('body-parser');
 
-app.set("view engine", "pug");
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.set('view engine', 'pug');
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => res.render('index'));
+app.get('/notes/create', (req, res) => res.render('create'));
+
+app.post('/notes/create', (req, res) => {
+	let notesDb = [];
+
+	fs.readFile(
+		path.join(__dirname, '/database/db.json'),
+		'utf-8',
+		(err, data) => {
+			if (err) console.log(err);
+			else notesDb = JSON.parse(data);
+
+			const note = {
+				title: req.body.title,
+				details: req.body.details,
+			};
+
+			notesDb.push(note);
+
+			fs.writeFile(
+				path.join(__dirname, '/database/db.json'),
+				JSON.stringify(notesDb),
+				(err) => {
+					if (err) res.redirect('/notes/create?error=1');
+					else res.redirect('/notes/create?success=1');
+				},
+			);
+		},
+	);
 });
 
-app.get("/notes/create", (req, res) => {
-  res.render("create");
+app.get('/notes', (req, res) => {
+	let db = [];
+	fs.readFile(
+		path.join(__dirname, '/database/db.json'),
+		'utf-8',
+		(err, data) => {
+			if (err) console.log(err);
+			else db = JSON.parse(data);
+
+			res.render('notes', { notes: db });
+		},
+	);
 });
 
-const notesDB = [];
-
-app.post("/notes/create", (req, res) => {
-  const note = {
-    title: req.body.title,
-    body: req.body.details,
-  };
-
-  fs.readFile(
-    path.join(__dirname, "/database/db.json"),
-    "utf-8",
-    (err, data) => {
-      if (err) {
-        console.error("Couldnt get the data from database");
-      } else {
-        notesDB = JSON.parse(data);
-      }
-    }
-  );
-
-  notesDB.push(note);
-
-  fs.writeFile(
-    path.join(__dirname, "/database/db.json"),
-    JSON.stringify(notesDB),
-    (err) => {
-      if (err) {
-        res.redirect("/notes/create?error=1");
-      } else {
-        res.redirect("/notes/create?succes=1");
-      }
-    }
-  );
-});
-
-app.get("/notes", (req, res) => {
-  res.render("notes", { notes: notesDB });
-});
-
-app.listen(8000, () => {
-  console.log("localhost:8000");
-});
+app.listen(3000, console.log('server is on localhost:3000'));
